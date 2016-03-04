12531d14b761d9fc30d3e0ec68dd4ec4fa7ab67e
Si Kai <sikai.ngsk@gmail.com>
2016-03-04 13:41:06 +0800

    Added skip pointers

--- merge.py
+++ merge.py
@@ -1,51 +1,76 @@
+import math
+
+
 def merge(list1, list2, op):
-	if (op == "AND"):
-		return merge_and(list1, list2)
-	elif (op == "OR"):
-		return merge_or(list1, list2)
+    if (op == "AND"):
+        return merge_and(list1, list2)
+    elif (op == "OR"):
+        return merge_or(list1, list2)
 
 
 def merge_and(list1, list2):
-	result = []
-	ptr1 = 0
-	ptr2 = 0
-	while ptr1 < len(list1) and ptr2 < len(list2):
-		if list1[ptr1] == list2[ptr2]:
-			result.append(list1[ptr1])
-			ptr1 += 1
-			ptr2 += 1
-		elif list1[ptr1] < list2[ptr2]:
-			ptr1 += 1
-		else:
-			ptr2 += 1
-	return result
+    result = []
+    ptr1 = 0
+    ptr2 = 0
+    list1_len = len(list1)
+    list1_skip = int(math.floor(math.sqrt(list1_len)))
+    list2_len = len(list2)
+    list2_skip = int(math.floor(math.sqrt(list2_len)))
+    while ptr1 < list1_len and ptr2 < list2_len:
+        if list1[ptr1] == list2[ptr2]:
+            result.append(list1[ptr1])
+            ptr1 += 1
+            ptr2 += 1
+        elif list1[ptr1] < list2[ptr2]:
+            # check for skip_pointer in list1
+            projected_skip = ptr1 + list1_skip
+            if ptr1 % list1_skip == 0 and projected_skip < list1_len - 1:
+                if list1[projected_skip] < list2[ptr2]:
+                    ptr1 = projected_skip
+                else:
+                    ptr1 += 1
+            else:
+                ptr1 += 1
+        else:
+            # check for skip_pointer in list2
+            projected_skip = ptr2 + list2_skip
+            if ptr2 % list2_skip == 0 and projected_skip < list2_len - 1:
+                if list2[projected_skip] < list1[ptr1]:
+                    ptr2 = projected_skip
+                else:
+                    ptr2 += 1
+            else:
+                ptr2 += 1
+    return result
 
 
 def merge_or(list1, list2):
-	result = []
-	ptr1 = 0
-	ptr2 = 0
-	while ptr1 < len(list1) and ptr2 < len(list2):
-		if list1[ptr1] == list2[ptr2]:
-			result.append(list1[ptr1])
-			ptr1 += 1
-			ptr2 += 1
-		elif list1[ptr1] < list2[ptr2]:
-			result.append(list1[ptr1])
-			ptr1 += 1
-		else:
-			result.append(list2[ptr2])
-			ptr2 += 1
-	while (ptr1 < len(list1)):
-		result.append(list1[ptr1])
-		ptr1 += 1
-	while (ptr2 < len(list2)):
-		result.append(list2[ptr2])
-		ptr2 += 1
-	return result
+    result = []
+    ptr1 = 0
+    ptr2 = 0
+    while ptr1 < len(list1) and ptr2 < len(list2):
+        if list1[ptr1] == list2[ptr2]:
+            result.append(list1[ptr1])
+            ptr1 += 1
+            ptr2 += 1
+        elif list1[ptr1] < list2[ptr2]:
+            result.append(list1[ptr1])
+            ptr1 += 1
+        else:
+            result.append(list2[ptr2])
+            ptr2 += 1
+    while (ptr1 < len(list1)):
+        result.append(list1[ptr1])
+        ptr1 += 1
+    while (ptr2 < len(list2)):
+        result.append(list2[ptr2])
+        ptr2 += 1
+    return result
 
 
-# first = [2, 4, 8, 16, 32, 64, 128]
+# first = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 16, 32, 64, 67, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 128]
 # second = [1, 2, 3, 5, 8, 13, 21, 34]
-# print merge_and(first, second)
+# third = [128, 129, 130, 131, 132, 133, 134, 135]
+# print len(first)
+# print merge_and(first, third)
 # print merge_or(first, second)
